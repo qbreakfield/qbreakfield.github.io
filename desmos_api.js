f072eaa5f11a29f74d3955499835ca386043898c
antiweevil <antiweevil@gmail.com>
2026-08-14 03:39:51 -0400

    improved organization and comments

--- desmos_api.js
+++ desmos_api.js
@@ -2,7 +2,7 @@
 let key;
 let storedKey = localStorage.getItem("desmos_api_key");
 
-// Prompt user for API key and save to localStorage
+// Function to prompt user for API key and save to localStorage
 function promptKey() {
     key = null;
     key = prompt("Enter your Desmos API key:");
@@ -10,16 +10,15 @@ function promptKey() {
     return key;
 }
 
+// Prompt user for key if needed, otherwise pull from localStorage
 let emptyStore = (storedKey === null || storedKey.trim() === "" || storedKey === "null");
 if (emptyStore) {
-    // Prompt user for key if not found in localStorage
     promptKey();
 } else {
-    // Use the stored key if available
     key = storedKey;
 }
 
-// Create script element and set its source to the Desmos API with the provided key
+// Function to create Desmos API script with the provided key
 function loadDesmosAPI(key, should_prompt) {
     let script = document.createElement('script');
     script.src = `https://www.desmos.com/api/v1.12/calculator.js?apiKey=${key}`;
@@ -31,15 +30,14 @@ function loadDesmosAPI(key, should_prompt) {
     // If the script fails to load, prompt the user for a new key and try again
     script.onerror = function(error) {
         console.warn("Desmos API refused to load.");
-        script.remove(); // Remove the failed script element
+        this.remove()
 
         if (should_prompt) {
             let new_key = promptKey(); // Prompt for a new key
             loadDesmosAPI(new_key, false); // Try loading again with the new key
         } else {
-            alert("Failed to load Desmos API. Please enter a valid API key.");
+            alert("Failed to load Desmos API. Please enter a valid API key."); // Otherwise, alert
         }
-        return;
     }
     
     document.head.appendChild(script);
--- script.js
+++ script.js
@@ -1,18 +1,22 @@
+// Parse JSON data
+const data = JSON.parse(item_data);
+
+// Configure MathJax
 window.MathJax = {
   tex: {
     inlineMath: {'[+]': [['@@', '@@']]}
   }
 };
 
-const data = JSON.parse(item_data);
+// Define and initiate global variables
 let answerRevealed = false;
 let desmosRevealed = false;
 let correctAnswer = -1;
 let freeResponse = false;
 let questionIndex = 0;
-
 let calculator;
 
+// Function to obtain all question indexes for a given unit name
 function getQuestionsByTopic(topic) {
     let matchingIndexes = [];
     for (let i = 0; i < data.length; i++) {
@@ -23,6 +27,7 @@ function getQuestionsByTopic(topic) {
     return matchingIndexes;
 }
 
+// Function to obtain all question indexes for a given difficulty
 function getQuestionsByDifficulty(difficulty) {
     let matchingIndexes = [];
     for (let i = 0; i < data.length; i++) {
@@ -33,82 +38,108 @@ function getQuestionsByDifficulty(difficulty) {
     return matchingIndexes;
 }
 
+// Function to attempt TeX formatting on an element
+function attemptTeXFormat(elem_id) {
+    try {
+        MathJax.typesetPromise([document.getElementById(elem_id)]);
+    } catch (err) {
+        console.error("MathJax typesetting failed:", err);
+    }
+}
+
+// Function to update question elements to match the JSON data for a given question index
 function importQuestion(data, index) {
+
+    // Hide solution by default
     answerRevealed = false;
+
+    // Store question index globally
     questionIndex = index;
 
+    // Initiate variables with question data
     let unit = data[index].unit;
     let prompt = data[index].text;
     let choices = data[index].opts;
-    freeResponse = (choices === null);
+
+    // If no choices are present, assume question is free response
     let correct;
+    freeResponse = (choices === null);
     if(freeResponse) {
         correct = data[index].crct;
     } else {
         correct = parseInt(data[index].crct);
     }
+    // Store correct answer globally
     correctAnswer = correct;
+
+    // Initiate variables with question data
     let difficulty = parseInt(data[index].diff);
     let id = data[index].id;
     let img_name = data[index].img;
     let graph_state = data[index].graph;
 
+    // Update unit and prompt with question data
     $('#q_unit').text(unit);
     $('#q_prompt').html(prompt);
-    try {
-        MathJax.typesetPromise([document.getElementById('q_prompt')]);
-    } catch (err) {
-        console.error("MathJax typesetting failed:", err);
-    }
+    attemptTeXFormat('q_prompt')
 
-    if(choices === null) {
-        $('#i_choices').addClass("hidden");
-        $('#i_free_choice').removeClass("hidden");
-        $('#i_free_choice').html("Free response.");
+    if(freeResponse) {
+        // If free response, only show one text element rather than a list
+        $('#q_choices').addClass("hidden");
+        $('#q_free_choice').removeClass("hidden");
+        $('#q_free_choice').html("Free response.");
     } else {
-        $('#i_choices').removeClass("hidden");
-        $('#i_free_choice').addClass("hidden");
+        // If multiple choice, show a list of options as answers
+        $('#q_choices').removeClass("hidden");
+        $('#q_free_choice').addClass("hidden");
 
-        for(let i=0; i<4; i++) {
-            $(`#chc${i+1}`).html(choices[i]);
-            try {
-                MathJax.typesetPromise([document.getElementById(`chc${i+1}`)]);
-            } catch (err) {
-                console.error("MathJax typesetting failed:", err);
-            }
-            $(`#chc${i+1}`).removeClass("correct_choice");
+        // Populate the text for each option
+        for(let i=1; i<=4; i++) {
+            $(`#q_chc${i}`).html(choices[i-1]);
+            attemptTeXFormat(`q_chc${i}`)
+            $(`#q_chc${i}`).removeClass("correct_choice");
         }
     }
 
-    for(let i=0; i<3; i++) {
-        if(i < difficulty) {
-            $(`#dft${i+1}`).addClass("i_filled");
+    // Update difficulty bar to match question data
+    for(let i=1; i<=3; i++) {
+        if(i <= difficulty) {
+            $(`#q_dft${i}`).addClass("q_filled");
         } else {
-            $(`#dft${i+1}`).removeClass("i_filled");
+            $(`#q_dft${i}`).removeClass("q_filled");
         }
     }
 
-    $('#i_id').text(`${id}`);
+    // Update the ID text with the question's ID
+    $('#q_id').text(`${id}`);
 
+    // If the question has an associated diagram, set the image's source
     if(img_name != null) {
-        $('#i_diagram').attr('src', `content/${img_name}`);
+        $('#q_diagram').attr('src', `content/${img_name}`);
     }
 
+    // If the question has an associated graph, set the calculator's state
     if(graph_state != null) {
         calculator.setState(graph_state);
     } else {
-        //$('#i_desmos').attr('disabled', true);
+        //$('#q_desmos').attr('disabled', true);
         calculator.setBlank();
     }
-    $('#calculator').addClass("hidden");
 
-    $('#i_toggle').removeClass("toggle_on");
-    $('#i_toggle').addClass("toggle_off");
-    $('#i_toggle').text("REVEAL");
+    // Show the diagram by default
+    $('#q_calculator').addClass("hidden");
+    $('#q_diagram').removeClass("hidden");
+
+    // Toggle the action button off by default
+    $('#q_toggle').removeClass("toggle_on");
+    $('#q_toggle').addClass("toggle_off");
+    $('#q_toggle').text("REVEAL");
 
     console.log("Question loaded successfully.");
+    return true;
 }
 
+// Function to promise to wait for the Desmos API to load, timing out on a wait too long
 function waitForDesmosAPI() {
     return new Promise((resolve, reject) => {
         if (typeof Desmos !== 'undefined' && Desmos.GraphingCalculator) {
@@ -129,76 +160,99 @@ function waitForDesmosAPI() {
     });
 }
 
+// Function that runs when the DOM fully loads
 $(document).ready(function() {
-    let elt = document.getElementById('calculator');
+    let elt = document.getElementById('q_calculator');
     
+    // Define and initiate a calculator variable once the Desmos API loads
     waitForDesmosAPI().then(() => {
         calculator = Desmos.GraphingCalculator(elt);
     }).catch((error) => {
         console.warn("Failed to load Desmos API:", error);
     });
 
-    $("#i_toggle").on("click", function() {
+    // Function to handle action button clicks, showing or hiding the question answer
+    $("#q_toggle").on("click", function() {
 
+        // Validate variable
         if(questionIndex < 0 || questionIndex >= data.length) {
             console.error("Invalid question index.");
             return;
         }
 
         if(answerRevealed) {
-            $('#i_toggle').removeClass("toggle_on");
-            $('#i_toggle').addClass("toggle_off");
-            $('#i_toggle').text("REVEAL");
-            answerRevealed = false;
+            // If the answer is currently visible, hide it
+            $('#q_toggle').removeClass("toggle_on");
+            $('#q_toggle').addClass("toggle_off");
+            $('#q_toggle').text("REVEAL");
 
-            if(freeResponse) {
-                $('#i_free_choice').text("Free response.");
-                $(`#i_free_choice`).removeClass("correct_choice");
-            } else {
-                $(`#chc${correctAnswer}`).removeClass("correct_choice");
+            if(freeResponse) { // Free response
+                $('#q_free_choice').text("Free response.");
+                $(`#q_free_choice`).removeClass("correct_choice");
+
+            } else { // Multiple choice
+                $(`#q_chc${correctAnswer}`).removeClass("correct_choice");
+
             }
             
         } else {
-            $('#i_toggle').removeClass("toggle_off");
-            $('#i_toggle').addClass("toggle_on");
-            $('#i_toggle').text("HIDE");
-            answerRevealed = true;
+            // Otherwise, the answer is currently hidden, so show it
+            $('#q_toggle').removeClass("toggle_off");
+            $('#q_toggle').addClass("toggle_on");
+            $('#q_toggle').text("HIDE");
 
-            if(freeResponse) {
-                $('#i_free_choice').html(`Answer: ${correctAnswer}`);
-                MathJax.typesetPromise([document.getElementById('i_free_choice')]);
-                $(`#i_free_choice`).addClass("correct_choice");
-            } else {
-                $(`#chc${correctAnswer}`).addClass("correct_choice");
+            if(freeResponse) { // Free response
+                $('#q_free_choice').html(`Answer: ${correctAnswer}`);
+                attemptTeXFormat('q_free_choice');
+                $(`#q_free_choice`).addClass("correct_choice");
+
+            } else { // Multiple choice
+                $(`#q_chc${correctAnswer}`).addClass("correct_choice");
+                
             }
 
         }
+        answerRevealed = !answerRevealed; // Toggle the variable
     });
 
-    $("#i_desmos").on("click", function() {
-        console.log(JSON.stringify(calculator.getState()));
+    // Function to handle Desmos button clicks, showing or hiding the graph
+    $("#q_desmos").on("click", function() {
+        console.log(JSON.stringify(calculator.getState())); // Debug purposes
+        
         if(desmosRevealed) {
-            $('#i_diagram').removeClass("hidden");
-            $('#calculator').addClass("hidden");
-            desmosRevealed = false;
+            // If the graph is currently visible, hide it
+            $('#q_diagram').removeClass("hidden");
+            $('#q_calculator').addClass("hidden");
+
         } else {
-            $('#i_diagram').addClass("hidden");
-            $('#calculator').removeClass("hidden");
-            desmosRevealed = true;
+            // Otherwise the graph is currently hidden, so show it
+            $('#q_diagram').addClass("hidden");
+            $('#q_calculator').removeClass("hidden");
+
         }
+        desmosRevealed = !desmosRevealed; // Toggle the variable
     });
 
+    // Function to handle topic clicks, opening a question of a given topic
     $(".c_topic").on("click", function() {
         let topic = $(this).text();
+
+        // Obtain question indexes for the topic
         let questions = getQuestionsByTopic(topic);
+
+        // Choose a random question index
         let randomQuestion = questions[Math.floor(Math.random() * questions.length)];
-        //let randomQuestion = questions[0]; // For testing
+        //let randomQuestion = questions[0]; // Debug purposes
+
+        // Validate the calculator
         if(!calculator) {
             alert("Desmos API failed to load. Please refresh the page and try again.");
             return;
         }
-        if(questions && questions.length > 0) {
-            importQuestion(data, randomQuestion);
+
+        // Import and show the question
+        if(randomQuestion && questions && questions.length > 0) {
+            let ready = importQuestion(data, randomQuestion);
             $('#question_bucket').removeClass("hidden");
             $('#contents_bucket').addClass("hidden");
         }
